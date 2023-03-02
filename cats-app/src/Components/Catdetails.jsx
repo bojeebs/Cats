10439b4bef620213d19adb439b8abdf94117cb4d
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-03-02 14:38:19 -0600

    test

--- cats-app/src/Components/Catdetails.jsx
+++ cats-app/src/Components/Catdetails.jsx
@@ -35,16 +35,43 @@ return cat ? (
       <h3 className="description">{cat.description}</h3>
       <h3>{cat.adaptability}</h3>
       
-      {[...Array(cat.affection_level)].map((star) => {  
-        return (      
-             <span className="star">&#9733;</span>)})}
+      <div>
+      Affection Level: {[...Array(cat.affection_level)].map(() => {  
+          return ( <span className="star">&#9733;</span>)})}
+      </div> 
+      <div>
+      Energy Level: {[...Array(cat.energy_level)].map(() => {  
+          return ( <span className="star">&#9733;</span>)})}
+      </div> 
+      <div>
+      Shedding Level: {[...Array(cat.shedding_level)].map(() => {  
+          return ( <span className="star">&#9733;</span>)})}
+      </div> 
+      <div>
+      Adaptability: {[...Array(cat.adaptability)].map(() => {  
+          return ( <span className="star">&#9733;</span>)})}
+      </div>
+      <div>
+      Child Friendly: {[...Array(cat.child_friendly)].map(() => {  
+          return ( <span className="star">&#9733;</span>)})}
+      </div>
+      <div>
+      Dog Friendly: {[...Array(cat.dog_friendly)].map(() => {  
+          return ( <span className="star">&#9733;</span>)})}
+      </div>
+      <div>
+      Intelligence: {[...Array(cat.intelligence)].map(() => {  
+          return ( <span className="star">&#9733;</span>)})}
+      </div>
+      <div>
+      Suppressed Tail: {[...Array(cat.suppressed_tail)].map(() => {  
+          return ( <span className="star">&#9733;</span>)})}
+      </div>
       <div className="buttons">
         <a href={cat.wikipedia_url} target="_blank" rel="noreferrer">
-          <button className="button">Wikipedia</button>
-        </a>
+          <button className="button">Wikipedia</button></a>
         <a href={cat.vetstreet_url} target="_blank" rel="noreferrer">
-          <button className="button">Vetstreet</button>
-        </a>
+          <button className="button">Vetstreet</button></a>
       </div>
     </div>
    
